c8e8b1c04169feae1c5dd93775ca5a65856ab388
feedan4 <fidanbagirova2004@gmail.com>
2024-10-25 19:52:37 +0400

    main bitirdik ama responsiv deyiul

--- src/main/Main.jsx
+++ src/main/Main.jsx
@@ -33,7 +33,7 @@ function Main() {
         </div>
       </div>
       <p className='text-right w-[95%] text-[#FF8300] cursor-pointer'>Hamısına bax</p>
-      <div className='w-[90%] h-[350px]  xl: lg:h-[300px] xl:h-[400px] m-[auto] mt-[200px] lg:mt-[200px] xl:mt-[50px] justify-evenly pb-[20px] md:flex  hidden '>
+      <div className='w-[90%] h-[350px]  xl: lg:h-[300px] xl:h-[400px] m-[auto] mt-[200px] lg:mt-[200px] xl:mt-[50px] justify-evenly pb-[20px] flex '>
         <div className='relative '>
           <p className='absolute bg-[#312f2fb3] text-[1.5em] p-[20px] text-white rounded-br-2xl '>Kategoriya: <br />Səhər yeməyi</p>
           <img className='h-[100%] rounded-2xl' src="https://neptun.az/image/catalog/bannerler/xususi-teklif/meyve-terevez.jpg" alt="" />
@@ -46,54 +46,202 @@ function Main() {
           <p className='absolute bg-[#312f2fb3] text-[1.5em] p-[20px] text-white rounded-br-2xl '>Kategoriya: <br />Səhər yeməyi</p>
           <img className='h-[100%] rounded-2xl ' src="https://neptun.az/image/catalog/YEN%C4%B0/MEHSUL/11.05.2020/cerez.jpg" alt="" />
         </div>
-
-
-        {/* <div className='flex gap-[30px] ml-[30px]'>
-          <div className='bg-white text-center rounded-2xl  *:py-[5px]'>
-            <img className='rounded-3xl' src="https://neptun.az/image/cache/catalog/meyve-terevez/qurumeyveler/096889-180x180.jpg?v=9" alt="" />
-            <p className='text-[.7em] font-[700]'>CER.ARMUD QURUSU KG</p>
-            <p className='text-[1.2em] font-[700]'>10.20 ₼</p>
+      </div>
+      <div className='w-[85%] mx-auto py-[50px]'>
+        <div className='flex justify-between items-center mb-[20px]'>
+          <div>Endirimli məhsullar</div>
+          <div className='text-[#FF8300]'>Hamısına bax</div>
+        </div>
+        <div className='flex gap-[30px] ml-[30px] flex-wrap lg:flex-nowrap'>
+          <div className='bg-white text-center rounded-2xl py-[25px] px-[10px] '>
+            <img className='rounded-3xl inline-block w-[100px]' src="https://neptun.az/image/cache/webp/catalog/OZMO20GREQQGERCEKSOKOLAD-1000x1000.webp?v=9" alt="" />
+            <p className='text-[.7em] font-[700]'>OZMO 20GR EQQ GERCEK SOKOLAD</p>
+            <div className='flex items-center justify-center gap-5'>
+              <div>
+                <button className='text-[12px] border rounded-full py-[8px] px-[3px] bg-[#FFD9C0]'>-18%</button>
+              </div>
+              <div>
+                <del className='text-[16px] text-gray-400'>2.00 ₼</del>
+                <p className='text-[22px]'>1.65 ₼</p>
+              </div>
+            </div>
             <div className='flex justify-center items-center '>
               <FaMinus className='text-[#FF8300] cursor-pointer' />
-                <span className='px-[10px]'>1 eded</span>
-              <FaPlus className='text-[#FF8300] cursor-pointer'/>
+              <span className='px-[10px]'>1 eded</span>
+              <FaPlus className='text-[#FF8300] cursor-pointer' />
             </div>
             <button className='text-white py-[3px] px-[15px] rounded-2xl bg-[#FF8300]'>Səbətə at</button>
           </div>
-          <div className='bg-white text-center rounded-2xl  *:py-[5px]'>
-            <img className='rounded-3xl' src="https://neptun.az/image/cache/catalog/meyve-terevez/qurumeyveler/096889-180x180.jpg?v=9" alt="" />
-            <p className='text-[.7em] font-[700]'>CER.ARMUD QURUSU KG</p>
-            <p className='text-[1.2em] font-[700]'>10.20 ₼</p>
+          <div className='bg-white text-center rounded-2xl py-[25px] px-[10px]'>
+            <img className='rounded-3xl inline-block w-[100px]' src="https://neptun.az/image/cache/webp/catalog/OZMO20GREQQGERCEKSOKOLAD-1000x1000.webp?v=9" alt="" />
+            <p className='text-[.7em] font-[700]'>OZMO 20GR EQQ GERCEK SOKOLAD</p>
+            <div className='flex items-center justify-center gap-5'>
+              <div>
+                <button className='text-[12px] border rounded-full py-[8px] px-[3px] bg-[#FFD9C0]'>-18%</button>
+              </div>
+              <div>
+                <del className='text-[16px] text-gray-400'>2.00 ₼</del>
+                <p className='text-[22px]'>1.65 ₼</p>
+              </div>
+            </div>
             <div className='flex justify-center items-center '>
               <FaMinus className='text-[#FF8300] cursor-pointer' />
-                <span className='px-[10px]'>1 eded</span>
-              <FaPlus className='text-[#FF8300] cursor-pointer'/>
+              <span className='px-[10px]'>1 eded</span>
+              <FaPlus className='text-[#FF8300] cursor-pointer' />
             </div>
             <button className='text-white py-[3px] px-[15px] rounded-2xl bg-[#FF8300]'>Səbətə at</button>
           </div>
-          <div className='bg-white text-center rounded-2xl  *:py-[5px]'>
-            <img className='rounded-3xl' src="https://neptun.az/image/cache/catalog/meyve-terevez/qurumeyveler/096889-180x180.jpg?v=9" alt="" />
-            <p className='text-[.7em] font-[700]'>CER.ARMUD QURUSU KG</p>
-            <p className='text-[1.2em] font-[700]'>10.20 ₼</p>
+          <div className='bg-white text-center rounded-2xl py-[25px] px-[10px]'>
+            <img className='rounded-3xl inline-block w-[100px]' src="https://neptun.az/image/cache/webp/catalog/OZMO20GREQQGERCEKSOKOLAD-1000x1000.webp?v=9" alt="" />
+            <p className='text-[.7em] font-[700]'>OZMO 20GR EQQ GERCEK SOKOLAD</p>
+            <div className='flex items-center justify-center gap-5'>
+              <div>
+                <button className='text-[12px] border rounded-full py-[8px] px-[3px] bg-[#FFD9C0]'>-18%</button>
+              </div>
+              <div>
+                <del className='text-[16px] text-gray-400'>2.00 ₼</del>
+                <p className='text-[22px]'>1.65 ₼</p>
+              </div>
+            </div>
             <div className='flex justify-center items-center '>
               <FaMinus className='text-[#FF8300] cursor-pointer' />
-                <span className='px-[10px]'>1 eded</span>
-              <FaPlus className='text-[#FF8300] cursor-pointer'/>
+              <span className='px-[10px]'>1 eded</span>
+              <FaPlus className='text-[#FF8300] cursor-pointer' />
             </div>
             <button className='text-white py-[3px] px-[15px] rounded-2xl bg-[#FF8300]'>Səbətə at</button>
           </div>
-          <div className='bg-white text-center rounded-2xl  *:py-[5px]'>
-            <img className='rounded-3xl' src="https://neptun.az/image/cache/catalog/meyve-terevez/qurumeyveler/096889-180x180.jpg?v=9" alt="" />
-            <p className='text-[.7em] font-[700]'>CER.ARMUD QURUSU KG</p>
-            <p className='text-[1.2em] font-[700]'>10.20 ₼</p>
+          <div className='bg-white text-center rounded-2xl py-[25px] px-[10px]'>
+            <img className='rounded-3xl inline-block w-[100px]' src="https://neptun.az/image/cache/webp/catalog/OZMO20GREQQGERCEKSOKOLAD-1000x1000.webp?v=9" alt="" />
+            <p className='text-[.7em] font-[700]'>OZMO 20GR EQQ GERCEK SOKOLAD</p>
+            <div className='flex items-center justify-center gap-5'>
+              <div>
+                <button className='text-[12px] border rounded-full py-[8px] px-[3px] bg-[#FFD9C0]'>-18%</button>
+              </div>
+              <div>
+                <del className='text-[16px] text-gray-400'>2.00 ₼</del>
+                <p className='text-[22px]'>1.65 ₼</p>
+              </div>
+            </div>
             <div className='flex justify-center items-center '>
               <FaMinus className='text-[#FF8300] cursor-pointer' />
-                <span className='px-[10px]'>1 eded</span>
-              <FaPlus className='text-[#FF8300] cursor-pointer'/>
+              <span className='px-[10px]'>1 eded</span>
+              <FaPlus className='text-[#FF8300] cursor-pointer' />
             </div>
             <button className='text-white py-[3px] px-[15px] rounded-2xl bg-[#FF8300]'>Səbətə at</button>
           </div>
-        </div> */}
+          <div className='bg-white text-center rounded-2xl py-[25px] px-[10px]'>
+            <img className='rounded-3xl inline-block w-[100px]' src="https://neptun.az/image/cache/webp/catalog/OZMO20GREQQGERCEKSOKOLAD-1000x1000.webp?v=9" alt="" />
+            <p className='text-[.7em] font-[700]'>OZMO 20GR EQQ GERCEK SOKOLAD</p>
+            <div className='flex items-center justify-center gap-5'>
+              <div>
+                <button className='text-[12px] border rounded-full py-[8px] px-[3px] bg-[#FFD9C0]'>-18%</button>
+              </div>
+              <div>
+                <del className='text-[16px] text-gray-400'>2.00 ₼</del>
+                <p className='text-[22px]'>1.65 ₼</p>
+              </div>
+            </div>
+            <div className='flex justify-center items-center '>
+              <FaMinus className='text-[#FF8300] cursor-pointer' />
+              <span className='px-[10px]'>1 eded</span>
+              <FaPlus className='text-[#FF8300] cursor-pointer' />
+            </div>
+            <button className='text-white py-[3px] px-[15px] rounded-2xl bg-[#FF8300]'>Səbətə at</button>
+          </div>
+          <div className='bg-white text-center rounded-2xl py-[25px] px-[10px]'>
+            <img className='rounded-3xl inline-block w-[100px]' src="https://neptun.az/image/cache/webp/catalog/OZMO20GREQQGERCEKSOKOLAD-1000x1000.webp?v=9" alt="" />
+            <p className='text-[.7em] font-[700]'>OZMO 20GR EQQ GERCEK SOKOLAD</p>
+            <div className='flex items-center justify-center gap-5'>
+              <div>
+                <button className='text-[12px] border rounded-full py-[8px] px-[3px] bg-[#FFD9C0]'>-18%</button>
+              </div>
+              <div>
+                <del className='text-[16px] text-gray-400'>2.00 ₼</del>
+                <p className='text-[22px]'>1.65 ₼</p>
+              </div>
+            </div>
+            <div className='flex justify-center items-center '>
+              <FaMinus className='text-[#FF8300] cursor-pointer' />
+              <span className='px-[10px]'>1 eded</span>
+              <FaPlus className='text-[#FF8300] cursor-pointer' />
+            </div>
+            <button className='text-white py-[3px] px-[15px] rounded-2xl bg-[#FF8300]'>Səbətə at</button>
+          </div>
+        </div>
+      </div>
+      <div className='w-[85%] mx-auto py-[50px] mt-[50px]'>
+        <div className='flex justify-between items-center mb-[20px]'>
+          <div>Ən çox satılanlar</div>
+          <div className='text-[#FF8300]'>Hamısına bax</div>
+        </div>
+        <div className='flex gap-[30px] ml-[30px] flex-wrap lg:flex-nowrap'>
+          <div className='bg-white text-center rounded-2xl py-[25px] px-[10px] px-[15px]'>
+            <img className='rounded-3xl inline-block w-[100px]' src="https://neptun.az/image/cache/webp/catalog/NEPISCHEVIEPRODUKTI/LICHNAYAGIGIENA/MILO/ProstiyeMila/021111-460x460.webp?v=9" alt="" />
+            <p className='text-[.7em] font-[700]'>DOVE 100GR SABUN COCONUT MILK</p>
+            <p className='text-[22px]'>2.35₼</p>
+            <div className='flex justify-center items-center '>
+              <FaMinus className='text-[#FF8300] cursor-pointer' />
+              <span className='px-[10px]'>1 eded</span>
+              <FaPlus className='text-[#FF8300] cursor-pointer' />
+            </div>
+            <button className='text-white py-[3px] px-[15px] rounded-2xl bg-[#FF8300]'>Səbətə at</button>
+          </div>
+          <div className='bg-white text-center rounded-2xl py-[25px] px-[10px]'>
+            <img className='rounded-3xl inline-block w-[100px]' src="https://neptun.az/image/cache/webp/catalog/NEPISCHEVIEPRODUKTI/LICHNAYAGIGIENA/MILO/ProstiyeMila/021111-460x460.webp?v=9" alt="" />
+            <p className='text-[.7em] font-[700]'>DOVE 100GR SABUN COCONUT MILK</p>
+            <p className='text-[22px]'>2.35₼</p>
+            <div className='flex justify-center items-center '>
+              <FaMinus className='text-[#FF8300] cursor-pointer' />
+              <span className='px-[10px]'>1 eded</span>
+              <FaPlus className='text-[#FF8300] cursor-pointer' />
+            </div>
+            <button className='text-white py-[3px] px-[15px] rounded-2xl bg-[#FF8300]'>Səbətə at</button>
+          </div>
+          <div className='bg-white text-center rounded-2xl py-[25px] px-[10px]'>
+            <img className='rounded-3xl inline-block w-[100px]' src="https://neptun.az/image/cache/webp/catalog/NEPISCHEVIEPRODUKTI/LICHNAYAGIGIENA/MILO/ProstiyeMila/021111-460x460.webp?v=9" alt="" />
+            <p className='text-[.7em] font-[700]'>DOVE 100GR SABUN COCONUT MILK</p>
+            <p className='text-[22px]'>2.35₼</p>
+            <div className='flex justify-center items-center '>
+              <FaMinus className='text-[#FF8300] cursor-pointer' />
+              <span className='px-[10px]'>1 eded</span>
+              <FaPlus className='text-[#FF8300] cursor-pointer' />
+            </div>
+            <button className='text-white py-[3px] px-[15px] rounded-2xl bg-[#FF8300]'>Səbətə at</button>
+          </div>
+          <div className='bg-white text-center rounded-2xl py-[25px] px-[10px]'>
+            <img className='rounded-3xl inline-block w-[100px]' src="https://neptun.az/image/cache/webp/catalog/NEPISCHEVIEPRODUKTI/LICHNAYAGIGIENA/MILO/ProstiyeMila/021111-460x460.webp?v=9" alt="" />
+            <p className='text-[.7em] font-[700]'>DOVE 100GR SABUN COCONUT MILK</p>
+            <p className='text-[22px]'>2.35₼</p>
+            <div className='flex justify-center items-center '>
+              <FaMinus className='text-[#FF8300] cursor-pointer' />
+              <span className='px-[10px]'>1 eded</span>
+              <FaPlus className='text-[#FF8300] cursor-pointer' />
+            </div>
+            <button className='text-white py-[3px] px-[15px] rounded-2xl bg-[#FF8300]'>Səbətə at</button>
+          </div>
+          <div className='bg-white text-center rounded-2xl py-[25px] px-[10px]'>
+            <img className='rounded-3xl inline-block w-[100px]' src="https://neptun.az/image/cache/webp/catalog/NEPISCHEVIEPRODUKTI/LICHNAYAGIGIENA/MILO/ProstiyeMila/021111-460x460.webp?v=9" alt="" />
+            <p className='text-[.7em] font-[700]'>DOVE 100GR SABUN COCONUT MILK</p>
+            <p className='text-[22px]'>2.35₼</p>
+            <div className='flex justify-center items-center '>
+              <FaMinus className='text-[#FF8300] cursor-pointer' />
+              <span className='px-[10px]'>1 eded</span>
+              <FaPlus className='text-[#FF8300] cursor-pointer' />
+            </div>
+            <button className='text-white py-[3px] px-[15px] rounded-2xl bg-[#FF8300]'>Səbətə at</button>
+          </div>
+          <div className='bg-white text-center rounded-2xl py-[25px] px-[10px]'>
+            <img className='rounded-3xl inline-block w-[100px]' src="https://neptun.az/image/cache/webp/catalog/NEPISCHEVIEPRODUKTI/LICHNAYAGIGIENA/MILO/ProstiyeMila/021111-460x460.webp?v=9" alt="" />
+            <p className='text-[.7em] font-[700]'>DOVE 100GR SABUN COCONUT MILK</p>
+            <p className='text-[22px]'>2.35₼</p>
+            <div className='flex justify-center items-center '>
+              <FaMinus className='text-[#FF8300] cursor-pointer' />
+              <span className='px-[10px]'>1 eded</span>
+              <FaPlus className='text-[#FF8300] cursor-pointer' />
+            </div>
+            <button className='text-white py-[3px] px-[15px] rounded-2xl bg-[#FF8300]'>Səbətə at</button>
+          </div>
+        </div>
       </div>
 
     </main>
